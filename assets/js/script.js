a5eb96fe208eb95d1e50396e8b3ce4e7f623f06f
Michael Tulmen <michaeltulmen@gmail.com>
2022-04-04 20:22:23 -0500

    script adolescence added ready function here paved way for api implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,22 @@ tailwind.config = {
       }
     }
 }
+
+$(document).ready(function() {
+    //preventDefault is a function that prevents the reloading of the page (relaod is the browser defualt action)
+
+    var amount
+    var from 
+    var to
+
+    $("#form").submit(fucntion(){
+        e.preventDefault();
+
+        amount = $("#amount").val();
+        
+        from = $("#from").val();
+
+        to = $("to").val();
+    })
+
+});
